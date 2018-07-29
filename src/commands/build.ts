4af58bf5d0c5d907f4f43872c85063e9e17775b0
Shea Rogers <shearogers@thinkforge.io>
2018-07-28 21:08:08 -0400

    fix some errors with the test

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,5 +1,5 @@
-import { Command, flags } from '@oclif/command'
-import { Module, i32 } from 'binaryen'
+import {Command, flags} from '@oclif/command'
+import {i32, Module} from 'binaryen'
 
 export default class Build extends Command {
   static description = 'Builds your .frg file(s).'
@@ -11,22 +11,22 @@ export default class Build extends Command {
   ]
 
   static flags = {
-    help: flags.help({ char: 'h' }),
+    help: flags.help({char: 'h'}),
     // flag with a value (-n, --name=VALUE)
-    name: flags.string({ char: 'o', description: 'output file' }),
+    name: flags.string({char: 'o', description: 'output file'}),
     // flag with no value (-f, --force)
-    force: flags.boolean({ char: 'f', description: 'force replace output files' }),
+    force: flags.boolean({char: 'f', description: 'force replace output files'}),
   }
 
-  static args = [{ name: 'file' }]
+  static args = [{name: 'file'}]
 
   async run() {
-    const { args, flags } = this.parse(Build)
+    const {args, flags} = this.parse(Build)
 
-    const myModule = new Module();
+    const myModule = new Module()
 
-    myModule.addFunction("add", myModule.addFunctionType("iii", i32, [i32, i32]), [i32],
-      myModule.block("test", [
+    myModule.addFunction('add', myModule.addFunctionType('iii', i32, [i32, i32]), [i32],
+      myModule.block('test', [
         myModule.setLocal(2,
           myModule.i32.add(
             myModule.getLocal(0, i32),
@@ -37,23 +37,23 @@ export default class Build extends Command {
           myModule.getLocal(2, i32)
         )
       ])
-    );
+    )
 
-    myModule.addFunctionExport("add", "add");
+    myModule.addFunctionExport('add', 'add')
 
     // Optimize the module using default passes and levels
-    myModule.optimize();
+    myModule.optimize()
     // Validate the module
     if (!myModule.validate())
-      throw new Error("validation error");
+      throw new Error('validation error')
 
     // Generate text format and binary
-    var textData = myModule.emitText();
-    var wasmData = myModule.emitBinary();
+    let textData = myModule.emitText()
+    let wasmData = myModule.emitBinary()
 
-    this.log(textData);
+    this.log('Build complete: ' + textData)
+    this.log('Build complete (wasm): ' + wasmData)
 
-    this.log(`hello from .\\src\\commands\\hello.ts`)
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
     }
--- test/commands/build.test.ts
+++ test/commands/build.test.ts
@@ -2,16 +2,9 @@ import {expect, test} from '@oclif/test'
 
 describe('build', () => {
   test
-  .stdout()
-  .command(['build'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
-
-  test
-  .stdout()
-  .command(['build', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    .stdout()
+    .command(['build'])
+    .it('returns text version of code', ctx => {
+      expect(ctx.stdout).to.contain('Build complete:')
+    })
 })
